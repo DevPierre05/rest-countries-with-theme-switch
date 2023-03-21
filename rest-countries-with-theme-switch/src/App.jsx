30c8281d67938f2e49a0268befb62f3612159bc6
DevPierre05 <pierrosei2009@yahoo.co.uk>
2023-03-21 23:16:26 +0000

    footer created

--- rest-countries-with-theme-switch/src/App.jsx
+++ rest-countries-with-theme-switch/src/App.jsx
@@ -152,200 +152,206 @@ function App() {
   };
 
   return (
-    <main className="min-h-screen dark:bg-[#111517] dark:text-gray-100 bg-[#fafafa]">
-      <div className="mx-auto md:max-w-8xl dark:bg-[#202c37]">
-        <header className="dark:bg-[#2b3945] bg-white px-2 shadow-md md:px-16">
-          <Header theme={theme} handleSwitch={handleSwitch} />
-        </header>
-        <main className="min-h-screen py-8 px-2 md:px-16">
-          {searchedCountry && (
-            <button
-              onClick={home}
-              className="flex items-center gap-2 ml-4 px-2 shadow-md rounded py-1 mb-2 dark:bg-[#2b3945] md:ml-0"
-            >
-              <span>
-                <IoHome />
-              </span>
-              Home
-            </button>
-          )}
+    <>
+      <main className="min-h-screen dark:bg-[#111517] dark:text-gray-100 bg-[#fafafa]">
+        <div className="mx-auto md:max-w-8xl dark:bg-[#202c37]">
+          <header className="dark:bg-[#2b3945] bg-white px-2 shadow-md md:px-16">
+            <Header theme={theme} handleSwitch={handleSwitch} />
+          </header>
+          <main className="min-h-screen py-8 px-2 md:px-16">
+            {searchedCountry && (
+              <button
+                onClick={home}
+                className="flex items-center gap-2 ml-4 px-2 shadow-md rounded py-1 mb-2 dark:bg-[#2b3945] md:ml-0"
+              >
+                <span>
+                  <IoHome />
+                </span>
+                Home
+              </button>
+            )}
 
-          {Object.keys(selectedCountryDiv).length === 0 &&
-          selectedCountryDiv.constructor === Object ? (
-            <div>
-              <div className="search flex flex-col gap-8 md:justify-between md:items-center md:flex-row">
-                <form onSubmit={submit} className="w-full">
-                  <input
-                    className="md:w-2/5 border-none cursor-pointer font-light dark:bg-[#2b3945] dark:placeholder-[#fafafa] p-4 md:p-4 text-sm rounded-md shadow-md outline-none"
-                    name="country"
-                    type="text"
-                    placeholder="Search for a country...."
+            {Object.keys(selectedCountryDiv).length === 0 &&
+            selectedCountryDiv.constructor === Object ? (
+              <div>
+                <div className="search flex flex-col gap-8 md:justify-between md:items-center md:flex-row">
+                  <form onSubmit={submit} className="w-full">
+                    <input
+                      className="md:w-2/5 border-none cursor-pointer font-light dark:bg-[#2b3945] dark:placeholder-[#fafafa] p-4 md:p-4 text-sm rounded-md shadow-md outline-none"
+                      name="country"
+                      type="text"
+                      placeholder="Search for a country...."
+                      onChange={handleChange}
+                      value={formData.country}
+                    />
+                  </form>
+
+                  <select
+                    className="dark:bg-[#2b3945] text-sm p-4 rounded-md w-2/5 md:w-1/3 shadow-md border-white outline-none"
+                    name="region"
+                    value={formData.region}
                     onChange={handleChange}
-                    value={formData.country}
-                  />
-                </form>
-
-                <select
-                  className="dark:bg-[#2b3945] text-sm p-4 rounded-md w-2/5 md:w-1/3 shadow-md border-white outline-none"
-                  name="region"
-                  value={formData.region}
-                  onChange={handleChange}
-                  required
-                >
-                  <option value="">Filter by Region</option>
-                  <option value="africa">Africa</option>
-                  <option value="americas">Americas</option>
-                  <option value="asia">Asia</option>
-                  <option value="europe">Europe</option>
-                  <option value="oceania">Oceania</option>
-                </select>
-              </div>
-
-              {/* No Search Results */}
-              {countryData.length === 0 && (
-                <div className="mt-16 w-full flex flex-col gap-6 items-center justify-center">
-                  <IoGlobeOutline className="w-20 h-20" />
-                  <h2 className="text-3xl text-center">Oooops...</h2>
-                  <h2 className="text-3xl text-center">{noResults}</h2>
+                    required
+                  >
+                    <option value="">Filter by Region</option>
+                    <option value="africa">Africa</option>
+                    <option value="americas">Americas</option>
+                    <option value="asia">Asia</option>
+                    <option value="europe">Europe</option>
+                    <option value="oceania">Oceania</option>
+                  </select>
                 </div>
-              )}
 
-              {/* Image and Details of Countries */}
-              <div className="grid-container gap-12">
-                {countryData.map((item, i) => {
-                  return (
-                    <article
-                      key={i}
-                      onClick={() => handleCountryClick(item.id)}
-                      className="mt-12 w-64 dark:bg-[#2b3945] rounded-t-md rounded-b-md shadow-md overflow-x-hidden"
-                    >
-                      <img
-                        src={item.flag}
-                        alt="country flag"
-                        className="w-64 h-44 object-cover"
-                      />
-                      <div className="mx-6 h-44 pb-10 leading-relaxed">
-                        <h2 className="mt-6 text-xl font-extrabold tracking-wide">
-                          {item.name}
-                        </h2>
-                        <p className="mt-3 text-sm font-semibold">
+                {/* No Search Results */}
+                {countryData.length === 0 && (
+                  <div className="mt-16 w-full flex flex-col gap-6 items-center justify-center">
+                    <IoGlobeOutline className="w-20 h-20" />
+                    <h2 className="text-3xl text-center">Oooops...</h2>
+                    <h2 className="text-3xl text-center">{noResults}</h2>
+                  </div>
+                )}
+
+                {/* Image and Details of Countries */}
+                <div className="grid-container gap-12">
+                  {countryData.map((item, i) => {
+                    return (
+                      <article
+                        key={i}
+                        onClick={() => handleCountryClick(item.id)}
+                        className="mt-12 w-64 dark:bg-[#2b3945] rounded-t-md rounded-b-md shadow-md overflow-x-hidden"
+                      >
+                        <img
+                          src={item.flag}
+                          alt="country flag"
+                          className="w-64 h-44 object-cover"
+                        />
+                        <div className="mx-6 h-44 pb-10 leading-relaxed">
+                          <h2 className="mt-6 text-xl font-extrabold tracking-wide">
+                            {item.name}
+                          </h2>
+                          <p className="mt-3 text-sm font-semibold">
+                            Population:{" "}
+                            <span className="dark:text-gray-400 text-gray-600">
+                              {item.population.toLocaleString()}
+                            </span>
+                          </p>
+                          <p className="mt-1 text-sm font-semibold">
+                            Region:{" "}
+                            <span className="dark:text-gray-400 text-gray-600">
+                              {item.region}
+                            </span>
+                          </p>
+                          <p className="mt-1 text-sm font-semibold">
+                            Capital:{" "}
+                            <span className="dark:text-gray-400 text-gray-600">
+                              {item.capital}
+                            </span>
+                          </p>
+                        </div>
+                      </article>
+                    );
+                  })}
+                </div>
+              </div>
+            ) : (
+              <div className="min-h-screen px-4 md:px-0">
+                <button
+                  onClick={backToMain}
+                  className="flex items-center gap-x-2 dark:bg-[#2b3945] dark:text-slate-300 mt-4 px-10 text-sm rounded-md py-2 shadow-lg"
+                >
+                  <span>
+                    <BsArrowLeft className="text-xl dark:text-white" />
+                  </span>
+                  Back
+                </button>
+
+                <div className="flex flex-col mt-16 mx-auto md:items-center md:justify-center md:gap-x-16 lg:gap-x-32 md:mt-20 md:flex-row">
+                  <div className="basis-full md:basis-5/12">
+                    <img
+                      src={selectedCountryDiv.flag}
+                      alt="country image flag"
+                      className="h-64 md:h-96"
+                    />
+                  </div>
+                  <div className="details mt-8 flex flex-col basis-full md:basis-7/12">
+                    <h1 className="text-2xl font-extrabold tracking-wide">
+                      {selectedCountryDiv.name}
+                    </h1>
+                    <div className="flex flex-col gap-4 mt-6 md:flex-row md:gap-x-12 lg:gap-x-28">
+                      <div className="">
+                        <p className="mt-3 text-md font-semibold">
+                          Native Name:{" "}
+                          <span className="dark:text-gray-400 text-gray-600">
+                            {selectedCountryDiv.nativeName}
+                          </span>
+                        </p>
+                        <p className="mt-3 text-md font-semibold">
                           Population:{" "}
                           <span className="dark:text-gray-400 text-gray-600">
-                            {item.population.toLocaleString()}
+                            {selectedCountryDiv.population.toLocaleString()}
                           </span>
                         </p>
-                        <p className="mt-1 text-sm font-semibold">
+                        <p className="mt-3 text-md font-semibold">
                           Region:{" "}
                           <span className="dark:text-gray-400 text-gray-600">
-                            {item.region}
+                            {selectedCountryDiv.region}
                           </span>
                         </p>
-                        <p className="mt-1 text-sm font-semibold">
+                        <p className="mt-3 text-md font-semibold">
+                          Sub Region:{" "}
+                          <span className="dark:text-gray-400 text-gray-600">
+                            {selectedCountryDiv.subregion}
+                          </span>
+                        </p>
+                        <p className="mt-3 text-md font-semibold">
                           Capital:{" "}
                           <span className="dark:text-gray-400 text-gray-600">
-                            {item.capital}
+                            {selectedCountryDiv.capital}
                           </span>
                         </p>
                       </div>
-                    </article>
-                  );
-                })}
-              </div>
-            </div>
-          ) : 
-           <div className="min-h-screen px-4 md:px-0">
-              <button
-                onClick={backToMain}
-                className="flex items-center gap-x-2 dark:bg-[#2b3945] dark:text-slate-300 mt-4 px-10 text-sm rounded-md py-2 shadow-lg"
-              >
-                <span>
-                  <BsArrowLeft className="text-xl dark:text-white" />
-                </span>
-                Back
-              </button>
-
-              <div className="flex flex-col mt-16 mx-auto md:items-center md:justify-center md:gap-x-16 lg:gap-x-32 md:mt-20 md:flex-row">
-                <div className="basis-full md:basis-5/12">
-                  <img
-                    src={selectedCountryDiv.flag}
-                    alt="country image flag"
-                    className="h-64 md:h-96"
-                  />
-                </div>
-                <div className="details mt-8 flex flex-col basis-full md:basis-7/12">
-                  <h1 className="text-2xl font-extrabold tracking-wide">
-                    {selectedCountryDiv.name}
-                  </h1>
-                  <div className="flex flex-col gap-4 mt-6 md:flex-row md:gap-x-12 lg:gap-x-28">
-                    <div className="">
-                      <p className="mt-3 text-md font-semibold">
-                        Native Name:{" "}
-                        <span className="dark:text-gray-400 text-gray-600">
-                          {selectedCountryDiv.nativeName}
-                        </span>
-                      </p>
-                      <p className="mt-3 text-md font-semibold">
-                        Population:{" "}
-                        <span className="dark:text-gray-400 text-gray-600">
-                          {selectedCountryDiv.population.toLocaleString()}
-                        </span>
-                      </p>
-                      <p className="mt-3 text-md font-semibold">
-                        Region:{" "}
-                        <span className="dark:text-gray-400 text-gray-600">
-                          {selectedCountryDiv.region}
-                        </span>
-                      </p>
-                      <p className="mt-3 text-md font-semibold">
-                        Sub Region:{" "}
-                        <span className="dark:text-gray-400 text-gray-600">
-                          {selectedCountryDiv.subregion}
-                        </span>
-                      </p>
-                      <p className="mt-3 text-md font-semibold">
-                        Capital:{" "}
-                        <span className="dark:text-gray-400 text-gray-600">
-                          {selectedCountryDiv.capital}
-                        </span>
-                      </p>
+                      <div className="">
+                        <p className="mt-3 text-md font-semibold">
+                          Top Level Domain:{" "}
+                          <span className="dark:text-gray-400 text-gray-600">
+                            {selectedCountryDiv.topLevelDomain[0]}
+                          </span>
+                        </p>
+                        <p className="mt-3 text-md font-semibold">
+                          Currencies:{" "}
+                          <span className="dark:text-gray-400 text-gray-600">
+                            {selectedCountryDiv.currencies[0].name}
+                          </span>
+                        </p>
+                        <p className="mt-3 text-md font-semibold">
+                          Languages:{" "}
+                          <span className="dark:text-gray-400 text-gray-600">
+                            {langString(selectedCountryDiv.languages)}
+                          </span>
+                        </p>
+                      </div>
                     </div>
-                    <div className="">
-                      <p className="mt-3 text-md font-semibold">
-                        Top Level Domain:{" "}
-                        <span className="dark:text-gray-400 text-gray-600">
-                          {selectedCountryDiv.topLevelDomain[0]}
-                        </span>
-                      </p>
-                      <p className="mt-3 text-md font-semibold">
-                        Currencies:{" "}
-                        <span className="dark:text-gray-400 text-gray-600">
-                          {selectedCountryDiv.currencies[0].name}
-                        </span>
-                      </p>
-                      <p className="mt-3 text-md font-semibold">
-                        Languages:{" "}
-                        <span className="dark:text-gray-400 text-gray-600">
-                          {langString(selectedCountryDiv.languages)}
-                        </span>
-                      </p>
-                    </div>
-                  </div>
 
-                  <div className="mt-6 flex gap-4 flex-col md:gap-2 items-baseline md:flex-row">
-                    <p className="mt-3 w-1/2 text-md font-semibold md:w-1/4">
-                      Border Countries:{" "}
-                    </p>
-                    <div className="grid grid-cols-3 gap-4 cursor-pointer md:grid-cols-3 md:gap-3">
-                      {borderDiv()}
+                    <div className="mt-6 flex gap-4 flex-col md:gap-2 items-baseline md:flex-row">
+                      <p className="mt-3 w-1/2 text-md font-semibold md:w-1/4">
+                        Border Countries:{" "}
+                      </p>
+                      <div className="grid grid-cols-3 gap-4 cursor-pointer md:grid-cols-3 md:gap-3">
+                        {borderDiv()}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          }
-        </main>
-      </div>
-    </main>
+            )}
+          </main>
+        </div>
+      </main>
+      <footer className="dark:bg-gray-600 dark:text-slate-200 text-center">
+        Challenge by <span><a className="underline"href="https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca">Frontend Mentor</a></span>, Coded by{" "}
+        <span><a className="underline" href="https://github.com/DevPierre05"target="_blank">Peter Osei</a></span>
+      </footer>
+    </>
   );
 }
 
